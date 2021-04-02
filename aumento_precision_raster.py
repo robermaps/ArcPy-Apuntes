844a718304fe74c42bc9a32efe9c62e16613ba0a
Roberto J <programapa.contacta@gmail.com>
2021-04-03 00:15:50 +0200

    Create aumento_precision_raster.py

---
+++ aumento_precision_raster.py
@@ -0,0 +1,51 @@
+# Este script permite aumentar la precisión de los datos de un ráster
+# repartiendo el valor original de cada píxel entre los nuevos píxeles
+# que genera el aumento de su resolución.
+#
+
+# Cargar capa entrada
+raster_entrada = Raster('prod')
+
+# Resoluciones
+resolucion_1 = int(raster_entrada.meanCellWidth)
+print('La resolución del ráster de entrada es ' + str(resolucion_1))
+resolucion_2 = 0 # No tocar
+
+# Función para obtener el número de píxeles que rellenará cada píxel
+# del ráster de entrada
+def numpix(lado1, lado2):
+    resultado = lado1 ** 2 / lado2 ** 2
+    print('Cada píxel se dividió en ' + str(resultado))
+    return resultado
+    resol_propuesta = []
+
+# Obtención de múltiplos
+for i in range(0, resolucion_1):
+    try:
+        if resolucion_1 % i == 0 and resolucion_1 % i == 0:
+            resol_propuesta.append(i)
+    except:
+        continue
+
+print('Puede cambiar la resolución a cualquiera de las siguientes: \n', resol_propuesta)
+
+# Nueva resolución
+while True:
+    try:
+        resolucion_2 = input('Introduce la resolución mínima necesaria: ')
+        if resolucion_2 not in resol_propuesta:
+            print('ERROR - Introduzca solo alguna de las siguientes resoluciones: \n', resol_propuesta)
+            continue
+        else:
+            break
+    except:
+        print('ERROR - Asegúrese de introducir un valor numérico')
+        continue
+
+# Aumento de la resolución
+print('Aumentando la resolución a ' + str(resolucion_2))
+arcpy.Resample_management(raster_entrada, ruta + 'raster_temp.tif', str(resolucion_2), 'NEAREST')
+raster_temp = Raster(ruta + 'raster_temp.tif') / numpix(resolucion_1, resolucion_2)
+raster_temp.save(ruta + 'raster_' + str(resolucion_2) + '.tif')
+nuevo_raster = Raster(ruta + 'raster_' + str(resolucion_2) + '.tif')
+ 
