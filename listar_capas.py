04f5f1b5c2d4c23add18ad7f64836972b7879fcf
Roberto J <programapa.contacta@gmail.com>
2021-04-07 11:44:05 +0200

    Create listar_capas.py

---
+++ listar_capas.py
@@ -0,0 +1,64 @@
+## Ejemplos para listar las capas del directorio de trabajo establecido en 
+## las variables de entorno en función de su nombre y su formato
+#
+
+# Modulos
+import arcpy
+ 
+# Entorno
+ruta = 'C:\\...'
+env.workspace = ruta
+
+
+##### --- VECTORIAL --- #####
+
+# Listar todas las capas vectoriales
+arcpy.ListFeatureClasses()
+ 
+# Listar solo las capas de puntos
+arcpy.ListFeatureClasses(,'Point')
+ 
+# Listar solo las capas de líneas
+arcpy.ListFeatureClasses(,'Line')
+ 
+# Listar solo las capas de polígonos
+arcpy.ListFeatureClasses(,'Polygon')
+ 
+# Listar solo las capas cuyo nombre empiece por 'Col'
+arcpy.ListFeatureClasses('Col*')
+ 
+# Listar solo las capas cuyo nombre termine por por 'egios'
+arcpy.ListFeatureClasses('*egios')
+ 
+# Listar solo las capas cuyo nombre coincida con 'Colegios'
+arcpy.ListFeatureClasses('Colegios')
+ 
+# Listar solo las capas cuyo nombre coincida con 'Colegios' y sean de tipo poligonal
+arcpy.ListFeatureClasses('Colegios', 'Polygon')
+
+
+##### --- RASTER --- #####
+
+# Listar todos los rasters
+arcpy.ListRasters()
+ 
+# Listar solo los rasters de tipo TIFF
+arcpy.ListRasters(,'tif')
+ 
+# Listar solo los rasters GRID
+arcpy.ListRasters(,'grid')
+ 
+# Listar solo los rasters IMG
+arcpy.ListRasters(,'img')
+ 
+# Listar solo los rasters cuyo nombre empiece por 'Temp'
+arcpy.ListRasters('Temp*')
+ 
+# Listar solo las capas cuyo nombre termine por por 'maximas'
+arcpy.ListRasters('*maximas')
+ 
+# Listar solo las capas cuyo nombre coincida con 'Temperaturas maximas'
+arcpy.ListRasters('Temperaturas maximas')
+ 
+# Listar solo las capas cuyo nombre empiece por 'Temp' y sean de tipo TIFF
+arcpy.ListRasters('Temp*', 'TIFF')
